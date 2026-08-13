8e47a64fcad9778b853d7464409a78123fd46ed9
zekegonzalez-dev <ezequielg070901@gmail.com>
2026-08-13 14:57:26 +0800

    JS Foundation Exercises 6 to 11

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,27 @@
-const sumAll = function() {
+const sumAll = function(num1,num2) {
+    let max;
+    let min;
+    
+    if(num1<0||num2<0||!Number.isInteger(num1)||!Number.isInteger(num2)){
+        return 'ERROR';
+    }
 
+    if(num1<num2){
+        min = num1;
+        max = num2;
+    }else{
+        min = num2;
+        max = num1;
+    }
+
+    let sum = 0;
+
+    for(i=min;i<=max;i++){
+        sum+=i;
+        console.log(sum);
+    }
+
+  return sum;
 };
 
 // Do not edit below this line
